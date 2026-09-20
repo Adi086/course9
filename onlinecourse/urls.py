5db9b777dc2a7719927ddf1ede80abd5e86a98ce
Adiboiii... <107349742+Adi086@users.noreply.github.com>
2026-09-20 13:43:39 +0530

    Add URL patterns for lesson submission and exam results

---
+++ onlinecourse/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path(
+        "submit/<int:lesson_id>/",
+        views.submit,
+        name="submit"
+    ),
+
+    path(
+        "exam-result/<int:lesson_id>/",
+        views.show_exam_result,
+        name="show_exam_result"
+    ),
+]
